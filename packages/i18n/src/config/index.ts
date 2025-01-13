268ab53255f6012601cc0aa884fc90b65bea18a7
opingcn <m16888@gmail.com>
2025-01-14 01:09:52 +0800

    Update index.ts
    Add Chinese language support.

--- packages/i18n/src/config/index.ts
+++ packages/i18n/src/config/index.ts
@@ -2,6 +2,7 @@ import en from "../locales/en/translations.json";
 import fr from "../locales/fr/translations.json";
 import es from "../locales/es/translations.json";
 import ja from "../locales/ja/translations.json";
+import ja from "../locales/cn/translations.json";
 
 export type Language = (typeof languages)[number];
 export type Translations = {
@@ -11,12 +12,13 @@ export type Translations = {
 };
 
 export const fallbackLng = "en";
-export const languages = ["en", "fr", "es", "ja"] as const;
+export const languages = ["en", "fr", "es", "ja","cn"] as const;
 export const translations: Translations = {
   en,
   fr,
   es,
   ja,
+  cn,
 };
 
 export const SUPPORTED_LANGUAGES = [
@@ -35,5 +37,9 @@ export const SUPPORTED_LANGUAGES = [
   {
     label: "Japanese",
     value: "ja",
+  },
+    {
+    label: "Chinese",
+    value: "cn",
   },
 ];
